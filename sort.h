8ae3a88ee72188d81911f56326f1cd5d95164cd7
Ademola Aina <ademic-aina@gmail.com>
2023-11-22 11:00:13 +0800

    0-bubble_sort.c

--- sort.h
+++ sort.h
@@ -1,7 +1,5 @@
 #ifndef _SORT_H_
 #define _SORT_H_
-void print_array(const int *array, size_t size)
-void print_list(const listint_t *list)
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -14,5 +12,8 @@ typedef struct listint_s
     const int n;
     struct listint_s *prev;
     struct listint_s *next;
-} listint_t
+} listint_t;
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 #endif /*_SORT_H_*/
