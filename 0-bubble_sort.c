d3576b4d789074f543b7206b81841228d2925893
Ademola Aina <ademic-aina@gmail.com>
2023-11-22 22:04:24 +0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,6 +19,8 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
+		if (array == NULL)
+			break;
 		swapped = false;
 		for (j = 0; j < size - i - 1; j++)
 		{
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list - a function that sorts a doubly linked list
+ * of integers in ascending order using the insertion sort algorithm
+ * @list: list to be sorted
+ */
